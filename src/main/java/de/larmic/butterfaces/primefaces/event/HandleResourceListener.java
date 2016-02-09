9438ab005d6a3422bcf851dfedb0daef2ab720b0
Lars Michaelis <mail@larmic.de>
2016-02-09 15:18:34 +0100

    simplify resource listener

--- src/main/java/de/larmic/butterfaces/primefaces/event/HandleResourceListener.java
+++ src/main/java/de/larmic/butterfaces/primefaces/event/HandleResourceListener.java
@@ -6,7 +6,6 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
-import java.util.Iterator;
 
 public class HandleResourceListener implements SystemEventListener {
 
@@ -17,15 +16,12 @@ public class HandleResourceListener implements SystemEventListener {
     public void processEvent(SystemEvent event) throws AbortProcessingException {
         final UIViewRoot source = (UIViewRoot) event.getSource();
         final FacesContext context = FacesContext.getCurrentInstance();
-        final Iterator<UIComponent> resourceIterator = source.getComponentResources(context, "head").iterator();
 
-        while (resourceIterator.hasNext()) {
-            final UIComponent resource = resourceIterator.next();
+        for (UIComponent resource : source.getComponentResources(context, "head")) {
             final String resourceLibrary = (String) resource.getAttributes().get("library");
             final String resourceName = (String) resource.getAttributes().get("name");
             if ("primefaces".equals(resourceLibrary) && "jquery/jquery.js".equals(resourceName)) {
                 source.removeComponentResource(context, resource);
-                resourceIterator.remove();
             }
         }
     }
