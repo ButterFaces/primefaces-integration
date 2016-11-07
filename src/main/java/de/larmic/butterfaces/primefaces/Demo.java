0745ab7b283c1db356368b865fb9f77b1b53d917
Lars Michaelis <mail@larmic.de>
2016-11-07 08:26:52 +0100

    add tags component

--- src/main/java/de/larmic/butterfaces/primefaces/Demo.java
+++ src/main/java/de/larmic/butterfaces/primefaces/Demo.java
@@ -17,6 +17,7 @@ public class Demo implements Serializable {
 
     private final List<String> autoCompleteValues = new ArrayList<String>();
     private String value;
+    private String tags;
     private int number;
 
     @PostConstruct
@@ -67,5 +68,13 @@ public class Demo implements Serializable {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
     }
 }
